22b13b39ce4af7bc3475a5c1461582f7cb30b8ec
Grace Joh <graceehjoh@gmail.com>
2023-04-18 16:14:40 -0500

    feat: create dishes/ingredients controller with create action

---
+++ app/controllers/dishes/ingredients_controller.rb
@@ -0,0 +1,8 @@
+class Dishes::IngredientsController < ApplicationController
+  def create
+    dish = Dish.find(params[:dish_id])
+    ingredient = Ingredient.find(params[:ingredient_id])
+    DishIngredient.create(dish: dish, ingredient: ingredient)
+    redirect_to dish_path(dish)
+  end
+end
