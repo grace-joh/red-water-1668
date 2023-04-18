19e76503773980bc0667b126eacb5c76990084ad
Grace Joh <graceehjoh@gmail.com>
2023-04-18 15:40:52 -0500

    test: add tests for Dish total_calories and chef_name

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @sam = Chef.create!(name: 'Sam')
+
+      @dish1 = @sam.dishes.create!(name: 'Breakfast Skillet', description: 'eggs, sausage, diced onions and cheddar cheese scrambled fresh in a hot skillet')
+  
+      @ing1 = Ingredient.create!(name: 'eggs', calories: 120)
+      @ing2 = Ingredient.create!(name: 'sausage', calories: 150)
+      @ing3 = Ingredient.create!(name: 'onion', calories: 100)
+      @ing4 = Ingredient.create!(name: 'cheddar cheese', calories: 150)
+  
+      @dish_ing1 = DishIngredient.create!(dish: @dish1, ingredient: @ing1)
+      @dish_ing2 = DishIngredient.create!(dish: @dish1, ingredient: @ing2)
+      @dish_ing3 = DishIngredient.create!(dish: @dish1, ingredient: @ing3)
+      @dish_ing4 = DishIngredient.create!(dish: @dish1, ingredient: @ing4)
+    end
+
+    describe '#total_calories' do
+      it 'returns the sum of all ingredient calories' do
+        expect(@dish1.total_calories).to eq(520)
+      end
+    end
+
+    describe '#chef_name' do
+      it 'returns the dishs chef name' do
+        expect(@dish1.chef_name).to eq(@sam.name)
+      end
+    end
+  end
 end
