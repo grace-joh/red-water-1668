680b0e620c1eaf3fe14620e9a57eec98d4634a80
Grace Joh <graceehjoh@gmail.com>
2023-04-18 16:49:42 -0500

    fix/feat: nest resources for dish ingredients and add resources for chefs and ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :dishes, only: [:show]
+  resources :dishes, only: [:show] do
+    resources :ingredients, only: [:create], controller: 'dishes/ingredients'
+  end
 
-  resources :dish_ingredients, only: [:create], controller: 'dishes/ingredients'
+  resources :chefs, only: [:show] do
+    resources :ingredients, only: [:index], controller: 'chefs/ingredients'
+  end
 end
